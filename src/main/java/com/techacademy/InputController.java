5a84f870ca282f5b5d6f33685cf74f6d95559063
hiroyuki <11hi01ro30yuki@gmail.com>
2023-12-16 16:17:46 +0900

    新規作成

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,15 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class InputController {
+    @GetMapping("/input")
+    public String getInput(@RequestParam(defaultValue="")String previous, Model model) {
+        model.addAttribute("previous", previous);
+        return "input";
+    }
+}
---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,16 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+
+@Controller
+public class OutputController {
+    @PostMapping("/output")
+    public String getInput(@RequestParam String val, Model model) {
+        model.addAttribute("val", val);
+        return "output";
+    }
+}
